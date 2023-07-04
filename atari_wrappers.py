66541226a104e616af21e7ea6ac45f3d59d1f749
AustinXZY <74691487+AustinXZY@users.noreply.github.com>
2023-07-03 20:31:39 -0400

    Update atari_wrappers.py

--- atari_wrappers.py
+++ atari_wrappers.py
@@ -56,10 +56,10 @@ class FireResetEnv(gym.Wrapper):
         self.env.reset(**kwargs)
         obs, _, done, _ = self.env.step(1)
         if done:
-            self.env.reset(**kwargs)
+            obs = self.env.reset(**kwargs)
         obs, _, done, _ = self.env.step(2)
         if done:
-            self.env.reset(**kwargs)
+            obs = self.env.reset(**kwargs)
         return obs
 
     def step(self, ac):
